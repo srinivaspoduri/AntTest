b1b24a6d2497fb1721c2bde07904741a2b8f025b
srini <srinu9tech@gmail.com>
2017-05-28 14:11:12 +0530

    tester

---
+++ src/Package1/TestClass.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	@Test
+	public void sample()
+	{
+		System.out.println("*****Sample in TestClass**********");
+	}
+
+}
