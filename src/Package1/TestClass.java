a1ff4a4eab232ca53e7a5b58272e1f7dafad0dc8
srinvaspoduri <srinu9tech@gmail.com>
2017-09-15 21:45:06 +0530

    Update TestClass.java

--- src/Package1/TestClass.java
+++ src/Package1/TestClass.java
@@ -11,5 +11,95 @@ public class TestClass {
 		System.out.println("*****Sample in TestClass**********");
 		System.out.println("*****Sample in TestClass**********");
 	}
+	{
+  "AWSTemplateFormatVersion":"2010-09-09",
+  "Description":"",
+  
+  "Parameters" : {
+    "AppID" : {
+      "Type" : "String"
+    } ,
+    "UserID" : {
+      "Type" : "String"
+    } ,
+    "Role" : {
+      "Type" : "String"
+    } ,
+	"InstanceName" : {
+      "Type" : "String"
+    } ,
+	"InstanceType" : {
+	  "Type" : "String"
+	}
+  },
+  
+  "Resources":{
+	    
+	"DynamicSeleniumDockerInstance":{
+      "Type":"AWS::EC2::Instance",
+      "Properties":{
+        "InstanceType": { "Ref" : "InstanceType" },
+        "ImageId": "ami-e0fbee99",
+        "SubnetId" : "subnet-0beb8d6c",
+		"SecurityGroupIds" : ["sg-f9e20b82", "sg-7a483b01", "sg-07b7c47c"],
+	    "Tags":[
+		{
+          "Key":"Userid",
+          "Value": { "Ref" : "UserID" }
+        },
+        {
+          "Key":"Vsad",
+          "Value": { "Ref" : "AppID" }
+        },
+		{
+          "Key":"Role",
+          "Value": { "Ref" : "Role" }
+        },		
+        {
+          "Key":"Name",
+          "Value": { "Ref" : "InstanceName" }
+        },
+        {
+          "Key":"nostop",
+          "Value": ""
+        }]
+      }
+    }
+  },
+
+  "Outputs":{
+    "InstanceId":{
+      "Description":"InstanceId of the newly created EC2 instance",
+      "Value":{   "Ref":"DynamicSeleniumDockerInstance"  }
+    },
+    "AZ":{
+      "Description":"Availability Zone of the newly created EC2 instance",
+      "Value":{
+        "Fn::GetAtt":[
+          "DynamicSeleniumDockerInstance",
+          "AvailabilityZone"
+        ]
+      }
+    },
+    "PrivateIP":{
+      "Description":"PrivateIP of the newly created EC2 instance",
+      "Value":{
+        "Fn::GetAtt":[
+          "DynamicSeleniumDockerInstance",
+          "PrivateIp"
+        ]
+      }
+    },
+    "PrivateDnsName":{
+      "Description": "Private DNS Name of the Instance",
+      "Value":{
+        "Fn::GetAtt":[
+          "DynamicSeleniumDockerInstance",
+          "PrivateDnsName"
+        ]
+      }
+    }
+  }
+}
 
 }
